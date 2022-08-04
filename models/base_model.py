ec14a432ea8b0ce25f192d600279266b8c96adef
Arsema-Zeweldi <arsugeb@gmail.com>
2022-08-04 08:28:58 -0700

    Task 4

--- models/base_model.py
+++ models/base_model.py
@@ -10,15 +10,20 @@ from json import JSONEncoder
 
 class BaseModel:
     """BaseModel"""
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-        models.storage.new(self)
-
-    def __str__(self):
-        class_name = self.__class__.__name__
-        return("[{}] ({}) {}".format(class_name, self.id, self.__dict__))
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, value in kwargs.items():
+                if key in ["created_at", "updated_at"]:
+                    value = datetime.strptime(
+                            value,
+                            '%Y-%m-%dT%H:%M:%S.%f')
+                if key not in ['__class__']:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def save(self):
         self.updated_at = datetime.now()
@@ -32,6 +37,10 @@ class BaseModel:
                 dict_repr[key] = value.strftime('%Y-%m-%dT%H:%M:%S.%f')
         dict_repr["__class__"] = type(self).__name__
         return dict_repr
+
+    def __str__(self):
+        class_name = self.__class__.__name__
+        return("[{}] ({}) {}".format(class_name, self.id, self.__dict__))
 
 
 class BaseModelEncoder(JSONEncoder):
