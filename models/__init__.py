82333c1fff24315ffd61103334c82547906b0271
Arsema-Zeweldi <arsugeb@gmail.com>
2022-08-05 00:51:53 -0700

    init

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,5 @@
 from models.base_model import BaseModel
-from models.Storage.file_storage import FileStorage
+from models.engine.file_storage import FileStorage
 from models.user import User
 from models.state import State
 from models.city import City
