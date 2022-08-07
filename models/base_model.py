f7eb830723eea9280737a66dd75ecd48d94a9731
Arsema-Zeweldi <arsugeb@gmail.com>
2022-08-07 03:27:59 -0700

    tests

--- models/base_model.py
+++ models/base_model.py
@@ -1,48 +1,54 @@
 #!/usr/bin/python3
-""" BaseModel Class """
+""" BaseModel class """
+
 
 import uuid
 from datetime import datetime
-from uuid import uuid4
 import models
-import json
-
-format_dt = "%Y-%m-%dT%H:%M:%S.%f"
+from json import JSONEncoder
 
 
 class BaseModel:
-    """ Basemodel class """
+    """BaseModel"""
     def __init__(self, *args, **kwargs):
-        if args is not None and len(args) > 0:
-            pass
         if kwargs:
-            for key, item in kwargs.items():
-                if key in ['created_at', 'updated_at']:
-                    item = datetime.strptime(item, format_dt)
+            for key, value in kwargs.items():
+                if key in ["created_at", "updated_at"]:
+                    value = datetime.strptime(
+                            value,
+                            '%Y-%m-%dT%H:%M:%S.%f')
                 if key not in ['__class__']:
-                    setattr(self, key, item)
+                    setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = self.updated_at = datetime.now()
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
             models.storage.new(self)
 
+    def save(self):
+        """ save method """
+        self.updated_at = datetime.now()
+        models.storage.save()
+
     def to_dict(self):
-        dic = {}
-        for key, item in self.__dict__.items():
-            if key in ['created_at', 'updated_at']:
-                dic[key] = item
-
-        dic['__class__'] = self.__class__.__name__
-        dic['created_at'] = self.created_at.isoformat()
-        dic['updated_at'] = self.updated_at.isoformat()
-        return dic
-
+        """ to_dict method """
+        dict_repr = self.__dict__.copy()
+        dict_repr["__class__"] = self.__class__.__name__
+        for key, value in self.__dict__.items():
+            if key in ("created_at", "updated_at"):
+                value = self.__dict__[key].isoformat()
+                dict_repr[key] = value
+        return dict_repr
 
     def __str__(self):
-        return("[{}] ({}) {}".format(self.__class__.__name__,
-                                     self.id, self.__dict__))
+        """ __str__ method """
+        class_name = self.__class__.__name__
+        return("[{}] ({}) {}".format(class_name, self.id, self.__dict__))
 
-    def save(self):
-        self.updated_at = datetime.now()
-        models.storage.new(self)
-        models.storage.save()
+
+class BaseModelEncoder(JSONEncoder):
+    """JSON"""
+    def default(self, o):
+        if isinstance(o, BaseModel):
+            return o.to_dict()
+        return super().default(o)
