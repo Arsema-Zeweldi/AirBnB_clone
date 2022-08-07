12eec7c4f6192bbc10ffbad060c1476ded316e82
Arsema-Zeweldi <arsugeb@gmail.com>
2022-08-07 04:38:49 -0700

    test storage

--- tests/test_models/engine/test_file_storage.py
+++ tests/test_models/engine/test_file_storage.py
@@ -4,6 +4,7 @@ Test file for the base_mode class
 """
 
 import unittest
+import pep8
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
 import os
@@ -11,60 +12,89 @@ import os
 
 class TestClass(unittest.TestCase):
     """Test cases"""
+    maxDiff = None
 
     def setUp(self):
-        self.storage = FileStorage()
-        self.model = BaseModel()
-        return super().setUp()
+        """ condition to test file saving """
+        with open("test.json", 'w'):
+            FileStorage._FileStorage__file_path = "test.json"
+            FileStorage._FileStorage__objects = {}
 
     def tearDown(self):
-        del(self.storage)
-        del(self.model)
-        if os.path.exists("file.json"):
-            os.remove("file.json")
-        return super().tearDown()
+        """ destroys created file """
+        FileStorage._FileStorage__file_path = "file.json"
+        try:
+            os.remove("test.json")
+        except FileNotFoundError:
+            pass
 
-    def test_is_instance(self):
-        """isInstance"""
+    def test_module_doc(self):
+        """ check for module documentation """
+        self.assertTrue(len(file_storage.__doc__) > 0)
 
-        self.assertIsInstance(self.storage, FileStorage)
+    def test_class_doc(self):
+        """ check for documentation """
+        self.assertTrue(len(FileStorage.__doc__) > 0)
 
-    def test_find_object_success(self):
+    def test_method_docs(self):
+        """ check for method documentation """
+        for func in dir(FileStorage):
+            self.assertTrue(len(func.__doc__) > 0)
 
-        self.storage.new(self.model)
-        self.assertIs(
-            self.storage.find('BaseModel', self.model.id), self.model
-            )
+    def test_pep8(self):
+        """ test base and test_base for pep8 conformance """
+        style = pep8.StyleGuide(quiet=True)
+        file1 = 'models/engine/file_storage.py'
+        file2 = 'tests/test_models/test_engine/test_file_storage.py'
+        result = style.check_files([file1, file2])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warning).")
 
-    def test_find_object_not_found(self):
+    def test_all(self):
+        """ Test method all from filestorage """
+        my_obj = FileStorage()
+        my_dict = my_obj.all()
+        self.assertTrue(type(my_dict) == dict)
 
-        self.storage.new(self.model)
-        self.assertRaisesRegex(
-            Exception,
-            'no instance found',
-            self.storage.find,
-            'BaseModel',
-            'does-not-exist')
+    def test_new(self):
+        """ Tests method new for filestorage """
+        my_obj = FileStorage()
+        new_obj = BaseModel()
+        my_obj.new(new_obj)
+        my_dict = my_obj.all()
+        key = "{}.{}".format(type(new_obj).__name__, new_obj.id)
+        self.assertTrue(key in my_dict)
 
-    def test_reset(self):
-        """reset"""
-        pass
+    def test_empty_reload(self):
+        """ Empty reload function """
+        my_obj = FileStorage()
+        new_obj = BaseModel()
+        my_obj.new(new_obj)
+        my_obj.save()
+        my_dict1 = my_obj.all()
+        os.remove("test.json")
+        my_obj.reload()
+        my_dict2 = my_obj.all()
+        self.assertTrue(my_dict2 == my_dict1)
 
-    def test_new_method(self):
-        """new"""
-        pass
+    def test_save(self):
+        """ Tests the save method for filestorage """
+        my_obj = FileStorage()
+        new_obj = BaseModel()
+        my_obj.new(new_obj)
+        my_dict1 = my_obj.all()
+        my_obj.save()
+        my_obj.reload()
+        my_dict2 = my_obj.all()
+        for key in my_dict1:
+            key1 = key
+        for key in my_dict2:
+            key2 = key
+        self.assertEqual(my_dict1[key1].to_dict(), my_dict2[key2].to_dict())
 
-    def test_save_method(self):
-        """save method"""
-        pass
-
-    def test_reload_function(self):
-        """reload function"""
-        pass
-
-    def test_function_all(self):
-        """all functions"""
-        pass
+    def test_instance(self):
+        """ Check storage """
+        self.assertIsInstance(storage, FileStorage)
 
 
 if __name__ == '__main__':
