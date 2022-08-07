7e18ae9864eede722c22015b420192fe130b85c0
Arsema-Zeweldi <arsugeb@gmail.com>
2022-08-07 04:26:00 -0700

    amenity test

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -4,59 +4,21 @@ Test file for user class
 """
 
 import unittest
-import pep8
 from models.amenity import Amenity
 from models.base_model import BaseModel
 
 
 class TestAmenityClass(unittest.TestCase):
     """Test cases"""
-    def tearDown(self):
-        """ destroys created file """
-        storage._FileStorage__file_path = "file.json"
-        try:
-            os.remove("test.json")
-        except FileNotFoundError:
-            pass
+    def test_create_istance(self):
+        """create a new instance"""
+        new_state = Review()
+        self.assertIsInstance(new_state, Review)
 
-    def setUp(self):
-        """Return to "" class attributes"""
-        with open("test.json", 'w'):
-            storage._FileStorage__file_path = "test.json"
-            storage._FileStorage__objects = {}
-        Amenity.name = ""
-
-    def test_module_doc(self):
-        """ check for module documentation """
-        self.assertTrue(len(amenity.__doc__) > 0)
-
-    def test_class_doc(self):
-        """ check for documentation """
-        self.assertTrue(len(Amenity.__doc__) > 0)
-
-    def test_method_docs(self):
-        """ check for method documentation """
-        for func in dir(Amenity):
-            self.assertTrue(len(func.__doc__) > 0)
-
-    def test_pep8(self):
-        """ test base and test_base for pep8 conformance """
-        style = pep8.StyleGuide(quiet=True)
-        file1 = 'models/amenity.py'
-        file2 = 'tests/test_models/test_amenity.py'
-        result = style.check_files([file1, file2])
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warning).")
-
-    def test_is_instance(self):
-        """ Test if user is instance of basemodel """
-        my_Amenity = Amenity()
-        self.assertTrue(isinstance(my_Amenity, BaseModel))
-
-    def test_field_types(self):
-        """ Test field attributes of user """
-        my_Amenity = Amenity()
-        self.assertTrue(type(my_Amenity.name) == str)
+    def test_create_istance2(self):
+        """create a new instance"""
+        new_state = Review()
+        self.assertIsInstance(new_state, BaseModel)
 
 
 if __name__ == '__main__':
