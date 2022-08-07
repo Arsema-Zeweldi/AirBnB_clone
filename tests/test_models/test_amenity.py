089c2929b8dd654ffa330221201fb894a719d113
Arsema-Zeweldi <arsugeb@gmail.com>
2022-08-07 04:32:46 -0700

    amenity test

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -12,12 +12,12 @@ class TestAmenityClass(unittest.TestCase):
     """Test cases"""
     def test_create_istance(self):
         """create a new instance"""
-        new_state = Review()
-        self.assertIsInstance(new_state, Review)
+        new_state = Amenity()
+        self.assertIsInstance(new_state, Amenity)
 
     def test_create_istance2(self):
         """create a new instance"""
-        new_state = Review()
+        new_state = Amenity()
         self.assertIsInstance(new_state, BaseModel)
 
 
