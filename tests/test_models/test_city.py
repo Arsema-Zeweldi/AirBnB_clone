4c2f010cb614944009c2addcd5f1f4a7e217d626
Arsema-Zeweldi <arsugeb@gmail.com>
2022-08-07 04:29:15 -0700

    city test

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -4,52 +4,36 @@ Test file for city class
 """
 
 import unittest
-import pep8
 from models.city import City
 from models.base_model import BaseModel
 
 
 class TestCityClass(unittest.TestCase):
     """Test cases"""
-    maxDiff = None
-
     def setUp(self):
-        """Return to "" class attributes"""
-        City.name = ""
-        City.state_id = ""
+        self.city = City()
+        return super().setUp()
 
-    def test_module_doc(self):
-        """ check for module documentation """
-        self.assertTrue(len(city.__doc__) > 0)
+    def tearDown(self):
+        del(self.city)
+        return super().tearDown()
 
-    def test_class_doc(self):
-        """ check for documentation """
-        self.assertTrue(len(City.__doc__) > 0)
+    def test_create_istance(self):
+        """create a new instance"""
+        self.assertIsInstance(self.city, City)
 
-    def test_method_docs(self):
-        """ check for method documentation """
-        for func in dir(City):
-            self.assertTrue(len(func.__doc__) > 0)
+    def test_create_istance_check_parent(self):
+        """check if it's instance of parent"""
+        self.assertIsInstance(self.city, BaseModel)
 
-    def test_pep8(self):
-        """ test base and test_base for pep8 conformance """
-        style = pep8.StyleGuide(quiet=True)
-        file1 = 'models/city.py'
-        file2 = 'tests/test_models/test_city.py'
-        result = style.check_files([file1, file2])
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warning).")
+    def test_class_attribut(self):
+        """initialze class attribute"""
+        self.city.name = "kigali"
+        self.assertIs(self.city.name, 'kigali')
 
-    def test_is_instance(self):
-        """ Test if user is instance of basemodel """
-        my_city = City()
-        self.assertTrue(isinstance(my_city, BaseModel))
-
-    def test_field_types(self):
-        """ Test field attributes of user """
-        my_city = City()
-        self.assertTrue(type(my_city.name) == str)
-        self.assertTrue(type(my_city.state_id) == str)
+    def test_parent_of_city(self):
+        """check if city is parent of BaseModel"""
+        self.assertEqual(isinstance(self.city, BaseModel), True)
 
 
 if __name__ == '__main__':
